c433f44453b043226e280b16b6f6471f61ba254b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2018-09-30 14:02:40 -0700

    trigger lock onclick and store in state

--- src/Lock.js
+++ src/Lock.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 import "./Lock.css";
 
+const LockIcon = ({ locked, onClickUpdateState }) => (
+  <span
+    className="lockIcon"
+    role="img"
+    aria-label="locked"
+    onClick={() => onClickUpdateState(!locked)}
+  >
+    {locked ? "🔒" : "🔓"}
+  </span>
+);
+
 class Lock extends Component {
+  state = {
+    locked: true
+  };
   render() {
     return (
       <div className="lockIconContainer">
-        <span className="lockIcon" role="img" aria-label="locked">
-          🔒
-        </span>
+        <LockIcon
+          locked={this.state.locked}
+          onClickUpdateState={locked => this.setState({ locked })}
+        />
       </div>
     );
   }
