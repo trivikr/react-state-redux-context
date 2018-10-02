0ebf925ad688a224c98aaae8874358a8db0caa26
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2018-10-01 21:17:10 -0700

    Add details about MobX while rewiring
    Link: https://github.com/timarney/react-app-rewired/tree/master/packages/react-app-rewire-mobx
    Downgrade to react-scripts@1.1.5 was required as there was some issue with decorators

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,6 @@
+const rewireMobX = require("react-app-rewire-mobx");
+
 module.exports = function override(config, env) {
-  //do stuff with the webpack config...
+  config = rewireMobX(config, env);
   return config;
 };
