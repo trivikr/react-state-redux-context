7572941822b2883983120c054b365512d9b09bbf
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2018-10-01 22:21:14 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React, { Component } from "react";
+import { action, observable } from "mobx";
+import { observer } from "mobx-react";
 import "./css/App.css";
 import Lock from "./Lock";
 import Smiley from "./Smiley";
 
-class App extends Component {
-  state = {
-    locked: true
-  };
+const LockStore = observable({ locked: false });
+LockStore.toggle = action(function() {
+  LockStore.locked = !LockStore.locked;
+});
 
+class App extends Component {
   render() {
-    const { locked } = this.state;
     return (
       <div className="App">
         <div className="iconContainer">
-          <Lock
-            locked={locked}
-            toggle={() => this.setState({ locked: !locked })}
-          />
+          <Lock locked={LockStore.locked} toggle={LockStore.toggle} />
         </div>
         <div className="iconContainer">
-          <Smiley locked={locked} />
+          <Smiley locked={LockStore.locked} />
         </div>
       </div>
     );
   }
 }
 
-export default App;
+export default observer(App);
