8a9312f95bdd7e3b3a52bc53d3484c58f97a4dbf
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2018-10-02 00:03:02 -0700

    Simplify code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import "./css/App.css";
 import Lock from "./Lock";
 import Smiley from "./Smiley";
 import { LockStore } from "./LockStore";
 
-class App extends Component {
-  render() {
-    return (
-      <LockStore>
-        <div className="App">
-          <div className="iconContainer">
-            <Lock />
-          </div>
-          <div className="iconContainer">
-            <Smiley />
-          </div>
-        </div>
-      </LockStore>
-    );
-  }
-}
+const App = () => (
+  <LockStore>
+    <div className="App">
+      <div className="iconContainer">
+        <Lock />
+      </div>
+      <div className="iconContainer">
+        <Smiley />
+      </div>
+    </div>
+  </LockStore>
+);
 
 export default App;
--- src/Lock.js
+++ src/Lock.js
@@ -1,24 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import "./css/Lock.css";
 import { LockContext } from "./LockStore";
 
-class Lock extends Component {
-  render() {
-    return (
-      <LockContext.Consumer>
-        {({ locked, toggle }) => (
-          <span
-            className="lockIcon"
-            role="img"
-            aria-label={`${locked ? "" : "un"}locked`}
-            onClick={toggle}
-          >
-            {locked ? "🔒" : "🔓"}
-          </span>
-        )}
-      </LockContext.Consumer>
-    );
-  }
-}
+const Lock = () => (
+  <LockContext.Consumer>
+    {({ locked, toggle }) => (
+      <span
+        className="lockIcon"
+        role="img"
+        aria-label={`${locked ? "" : "un"}locked`}
+        onClick={toggle}
+      >
+        {locked ? "🔒" : "🔓"}
+      </span>
+    )}
+  </LockContext.Consumer>
+);
 
 export default Lock;
